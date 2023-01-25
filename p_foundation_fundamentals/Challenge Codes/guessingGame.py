0422ecae058d4e2a62782494a736ab8ed90eac4d
Nikhil-Singla <nsingla3.14@gmail.com>
2023-01-25 00:44:56 -0600

    Challenge code

---
+++ p_foundation_fundamentals/Challenge Codes/guessingGame.py
@@ -0,0 +1,9 @@
+instream = input("What's my favourite food?: ")
+if instream == "Chicken":
+    print("Yep! So amazing!")
+else:
+    print("Yuck! That's not it!")
+
+print("Thanks for playing!")
+
+# CODE COMPLETED
